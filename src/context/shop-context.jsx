09c2777645a698f778cc8388e89825e88ced3989
masqualerro <86025634+masqualerro@users.noreply.github.com>
2023-01-30 17:45:43 -0800

    Update shop-context.jsx

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -2,6 +2,8 @@ import React, { createContext } from 'react';
 import { useState } from 'react';
 import { productList } from '../products';
 
+// context api allows data to be shared "globally" -- I am using this for the universal "cart array" and subsequent functions like "addToCart" and "getTotalCartAmount" to be present in product and shop components
+
 export const ShopContext = createContext(null);
 
 const getCart = () => {
